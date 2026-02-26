7b62b8dcc05a506240713ff2ed3acb127c9bcbec
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-02-26 19:47:31 +0700

    Initial commit

---
+++ src/main/java/ru/sibsutis/lab_results/LabResultsApplication.java
@@ -0,0 +1,13 @@
+package ru.sibsutis.lab_results;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabResultsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabResultsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sibsutis/lab_results/LabResultsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sibsutis.lab_results;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabResultsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
