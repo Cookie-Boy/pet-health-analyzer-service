9f2383c1d725f0f8db51d6f4bec87789e25f0ace
Vitaly Medvedenko <vitalik.med2018@gmail.com>
2026-05-05 21:04:38 +0700

    Update RestClientConfig

--- src/main/java/ru/sibsutis/pet_health_analyzer/configuration/RestClientConfig.java
+++ src/main/java/ru/sibsutis/pet_health_analyzer/configuration/RestClientConfig.java
@@ -1,15 +1,19 @@
 package ru.sibsutis.pet_health_analyzer.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
+    @Value("${gateway.url}")
+    private String url;
 
     @Bean
     public RestClient restClient() {
         return RestClient.builder()
+                .baseUrl(url)
                 .defaultHeader("Content-Type", "application/json")
                 .requestInterceptor((request, body, execution) -> execution.execute(request, body))
                 .build();
